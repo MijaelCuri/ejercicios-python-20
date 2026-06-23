f64e36695169080d0894d222c9be01f29ed32eb1
MijaelCuri <apsti71229329@jma.edu.pe>
2026-06-23 10:55:36 -0600

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,5 @@
+edad = int(input("Ingresa tu edad: "))
+if edad >= 18:
+    print("Eres Mayor de edad.")
+else:
+    print("Eres Menor de edad.")
---
+++ 14.py
@@ -0,0 +1,8 @@
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+if num1 > num2:
+    print(f"El primer número ({num1}) es mayor que el segundo ({num2}).")
+elif num2 > num1:
+    print(f"El segundo número ({num2}) es mayor que el primer ({num1}).")
+else:
+    print("Ambos números son iguales.")
